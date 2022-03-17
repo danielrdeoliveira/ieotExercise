20c0a64fe748cca7391dc3c65c1ec4205f83de15
danielrdeoliveira <101601633+danielrdeoliveira@users.noreply.github.com>
2022-03-17 18:22:29 -0300

    Final Version

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from employee import Employee
 if __name__ == '__main__':
     f = File()
     employee_dic = f.read_employees(filename="schedules.txt")
-    print(employee_dic)
     employee_list = [Employee(k, v) for (k, v) in employee_dic.items()]
 
     for emp in employee_list:
