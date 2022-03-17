dbfdcab0e5d8fca1a9e9e9670e8a3fb04b7ae6df
danielrdeoliveira <101601633+danielrdeoliveira@users.noreply.github.com>
2022-03-17 00:32:11 -0300

    Adjust Exception

--- employee.py
+++ employee.py
@@ -6,8 +6,8 @@ class Employee:
     payment = None
     interval_edge_1 = datetime(year=1900, month=1, day=1, hour=9, minute=1)
     interval_edge_2 = datetime(year=1900, month=1, day=1, hour=18, minute=1)
-    days_dict = {'MO': 0, 'TU': 1, 'WE': 2, 'TH': 3, 'FR': 4, 'SA': 5, 'SU': 6}
-    wages = {'W1': 25 / 60, 'W2': 15 / 60, 'W3': 20 / 60, 'W4': 30 / 60, 'W5': 20 / 60, 'W6': 25 / 60}
+    days_dict = {"MO": 0, "TU": 1, "WE": 2, "TH": 3, "FR": 4, "SA": 5, "SU": 6}
+    wages = {"W1": 25 / 60, "W2": 15 / 60, "W3": 20 / 60, "W4": 30 / 60, "W5": 20 / 60, "W6": 25 / 60}
 
     def __init__(self, name, hours_worked):
         self.name = name
@@ -21,13 +21,13 @@ class Employee:
 
         for day_hours in self.hours_worked:
 
-            day_start = datetime.strptime(day_hours['Start'], '%H:%M')
-            day_end = datetime.strptime(day_hours['End'], '%H:%M')
+            day_start = datetime.strptime(day_hours["Start"], "%H:%M")
+            day_end = datetime.strptime(day_hours["End"], "%H:%M")
 
-            if self.days_dict[day_hours['Day']] < 5:
-                v1, v2, v3 = self.wages['W1'], self.wages['W2'], self.wages['W3']
+            if self.days_dict[day_hours["Day"]] < 5:
+                v1, v2, v3 = self.wages["W1"], self.wages["W2"], self.wages["W3"]
             else:
-                v1, v2, v3 = self.wages['W4'], self.wages['W5'], self.wages['W6']
+                v1, v2, v3 = self.wages["W4"], self.wages["W5"], self.wages["W6"]
 
             if day_start < self.interval_edge_1:
                 if day_end < self.interval_edge_1:
@@ -56,8 +56,8 @@ class Employee:
 
         for day_hours in self.hours_worked:
 
-            day_start = datetime.strptime(day_hours['Start'], '%H:%M')
-            day_end = datetime.strptime(day_hours['End'], '%H:%M')
+            day_start = datetime.strptime(day_hours["Start"], '%H:%M')
+            day_end = datetime.strptime(day_hours["End"], '%H:%M')
 
             if day_end < day_start:
-                raise Exception('Schedule error for {} on {}'.format(self.name, day_hours['Day']))
+                raise Exception("Schedule error for {} on {}".format(self.name, day_hours["Day"]))
